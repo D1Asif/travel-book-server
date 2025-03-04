17b8dc7af463b1dbb093909f01aa9142cfbfdcef
Dewan Asifur Rahman <dasifrahman@gmail.com>
2025-03-04 08:23:01 +0600

    post comment timestampt populate attempt 3

--- src/app/modules/post/post.service.ts
+++ src/app/modules/post/post.service.ts
@@ -76,7 +76,7 @@ const getPostByIdFromDB = async (postId: string) => {
         })
         .populate({
             path: 'comments',
-            // select: '_id author content +createdAt +updatedAt',
+            select: 'author content createdAt updatedAt',
             populate: {
                 path: 'author',
                 select: '_id name username profilePicture isVerifiedUser'
